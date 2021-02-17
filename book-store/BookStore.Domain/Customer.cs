290be93c95995f5f063326b7a69cfb04d4f70ba7
John Werner <jwerner547@gmail.com>
2021-02-17 09:12:43 -0700

    updated customer model and tests

--- book-store/BookStore.Domain/Customer.cs
+++ book-store/BookStore.Domain/Customer.cs
@@ -9,17 +9,17 @@ namespace BookStore.Domain
             FirstName = firstName;
             LastName = lastName;
         }
-        
-        public Customer(string firstName, string lastName, int defaultStoreId)
+
+        public Customer(string firstName, string lastName, int defaultLocationId)
         {
             FirstName = firstName;
             LastName = lastName;
-            DefaultStoreID = defaultStoreId;
+            DefaultLocationID = defaultLocationId;
         }
 
         public int ID {get;}
-        public string FirstName {get;}
-        public string LastName {get;}
-        public int DefaultStoreID {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public int DefaultLocationID {get; set;}
     }
 }
--- book-store/BookStore.Tests/CustomerTests.cs
+++ book-store/BookStore.Tests/CustomerTests.cs
@@ -6,31 +6,65 @@ namespace BookStore.Tests
 {
     public class CustomerTests
     {
-        // private Customer c;
-        // private string testFirstName;
-        // private string testLastName;
+        private Customer c;
+        private string testFirstName = "Scrooge";
+        private string testLastName = "McDuck";
+        private int testLocationID = 1;
 
-        // public CustomerTests()
-        // {
-        //     testFirstName = "John";
-        //     testLastName = "Doe";
-        //     c = new Customer(testFirstName, testLastName);
-        // }
+        public CustomerTests()
+        {
+            c = new Customer(testFirstName, testLastName, testLocationID);
+        }
 
-        // [Fact]
-        // public void Customer_GetFirstName()
-        // {
-        //     string cfn = c.FirstName;
+        // test first name
+        [Fact]
+        public void Customer_TestGetFirstName()
+        {
+            string s = c.FirstName;
+            Assert.Equal(testFirstName, s);
+        }
 
-        //     Assert.Equal(testFirstName, cfn);
-        // }
+        [Fact]
+        public void Customer_TestSetFirstName()
+        {
+            string n = "David";
+            c.FirstName = n;
+            string s = c.FirstName;
+            Assert.Equal(n, s);
+        }
 
-        // [Fact]
-        // public void Customer_GetLastName()
-        // {
-        //     string cln = c.LastName;
+        // test last name
+        [Fact]
+        public void Customer_TestGetLastName()
+        {
+            string s = c.LastName;
+            Assert.Equal(testLastName, s);
+        }
 
-        //     Assert.Equal(testLastName, cln);
-        // }
+        [Fact]
+        public void Customer_TestSetLastName()
+        {
+            string n = "Rose";
+            c.LastName = n;
+            string s = c.LastName;
+            Assert.Equal(n, s);
+        }
+
+        // test default location id
+        [Fact]
+        public void Customer_TestGetDefaultLocationID()
+        {
+            int i = c.DefaultLocationID;
+            Assert.Equal(testLocationID, i);
+        }
+
+        [Fact]
+        public void Customer_TestSetDefaultLocationID()
+        {
+            int j = 77;
+            c.DefaultLocationID = j;
+            int i = c.DefaultLocationID;
+            Assert.Equal(j, i);
+        }
     }
 }
